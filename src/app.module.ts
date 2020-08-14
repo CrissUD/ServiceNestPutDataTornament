cef360f4c7e0ab7e4fbd6bdeeeb862af78573d17
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 13:38:01 -0500

    generacion servicios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PutTeamDataController } from './controllers/put-team-data/put-team-data.controller';
 import { PutMatchDataController } from './controllers/put-match-data/put-match-data.controller';
+import { TeamService } from './services/team/team.service';
+import { MatchService } from './services/match/match.service';
 
 @Module({
   imports: [],
   controllers: [AppController, PutTeamDataController, PutMatchDataController],
-  providers: [AppService],
+  providers: [AppService, TeamService, MatchService],
 })
 export class AppModule {}
