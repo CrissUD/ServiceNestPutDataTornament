c220764693a427a1c3133a023e13445d421f2b16
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 14:01:28 -0500

    se generan metdosos en controladores

--- src/controllers/put-match-data/put-match-data.controller.ts
+++ src/controllers/put-match-data/put-match-data.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Put, Body, Param } from '@nestjs/common';
+import { MatchService } from 'src/services/match/match.service';
 
 @Controller('put-match-data')
-export class PutMatchDataController {}
+export class PutMatchDataController {
+
+    constructor (private matchService: MatchService) {}
+    
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    @Put('/:idTournament/:idMatch')
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    modifyTornero(@Body() tournament, @Param('idTournament') idTournament, @Param('idMatch') idMatch) {
+        return this.matchService.modifyMatch(tournament, idTournament, idMatch);
+    }
+}
--- src/controllers/put-team-data/put-team-data.controller.ts
+++ src/controllers/put-team-data/put-team-data.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Put, Body, Param } from '@nestjs/common';
+import { TeamService } from 'src/services/team/team.service';
 
-@Controller('put-team-data')
-export class PutTeamDataController {}
+@Controller('team')
+export class PutTeamDataController {
+
+    constructor (private teamService: TeamService) {}
+    
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    @Put('/:idTournament/:idTeam')
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    modifyTornero(@Body() tournament, @Param('idTournament') idTournament, @Param('idTeam') idTeam) {
+        return this.teamService.modifyTeam(tournament, idTournament, idTeam);
+    }
+}
