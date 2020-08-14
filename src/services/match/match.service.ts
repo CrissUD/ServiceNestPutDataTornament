d7faacaac8a812e078ae5fb71f04efdd838e2dfb
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 13:57:33 -0500

    Se generan métdos en servicios

--- src/services/match/match.service.ts
+++ src/services/match/match.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { FirebaseService } from '../firebase/firebase.service';
 
 @Injectable()
-export class MatchService {}
+export class MatchService {
+
+    constructor(private firebaseService: FirebaseService) { }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    async modifyMatch(match, idTournament, idMatch) {
+        return await this.firebaseService.modifyElement(match, `/json/${idTournament}/matches/${idMatch}`);
+    }
+}
--- src/services/team/team.service.ts
+++ src/services/team/team.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { FirebaseService } from '../firebase/firebase.service';
 
 @Injectable()
-export class TeamService {}
+export class TeamService {
+
+    constructor(private firebaseService: FirebaseService) { }
+    
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    async modifyTeam (team, idTournament, idteam) {
+        return await this.firebaseService.modifyElement(team, `/json/${idTournament}/teams/${idteam}`);
+    }
+}
