ea411935945b86f74e9d3ce7ada057da54f2f4b5
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 13:45:29 -0500

    se agrega recepcion de credenciales

--- src/services/firebase/firebase.service.ts
+++ src/services/firebase/firebase.service.ts
@@ -4,6 +4,11 @@ import * as firebase from "firebase-admin";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const serviceAccount = require("../../../soccerCup.json");
 
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://soccercup-b6117.firebaseio.com"
+});
+
 @Injectable()
 export class FirebaseService {
 
