f334e0d26540cf3894f6766a8470516aced6e378
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 14:59:25 -0500

    Se implementa logica de negocio en servicio

--- src/services/match/match.service.ts
+++ src/services/match/match.service.ts
@@ -1,13 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
 import { FirebaseService } from '../firebase/firebase.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+const URL = 'http://localhost:3005/api/match/';
 
 @Injectable()
 export class MatchService {
 
-    constructor(private firebaseService: FirebaseService) { }
+    constructor(private http: HttpService, private firebaseService: FirebaseService) { }
 
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    async modifyMatch(match, idTournament, idMatch) {
-        return await this.firebaseService.modifyElement(match, `/json/${idTournament}/matches/${idMatch}`);
+    async modifyMatch(matchData, idTournament, idMatch) {
+        console.log(`${URL}${idTournament}/${idMatch}`);
+        this.findMatch(`${URL}${idTournament}/${idMatch}`).subscribe(data => {
+            console.log(data.data);
+        });
+        // return await this.firebaseService.modifyElement(match, `/json/${idTournament}/matches/${idMatch}`);
+    }
+
+    findMatch(url): Observable<any> {
+        return this.http.get(url);
     }
 }
